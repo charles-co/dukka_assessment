1aa5e4d4f7fc2910cf3561386e1cce7a948d3b76
Oraegbu Charles <oraegbucharlesc@gmail.com>
2023-01-30 23:51:47 +0100

    initial commit

---
+++ backend/dukka_assessment/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from dukka_assessment.users.models import User
+from dukka_assessment.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ backend/dukka_assessment/users/models.py
@@ -0,0 +1,85 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db.models import CharField, EmailField
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+
+    Male = "M"
+    Female = "F"
+    Other = "O"
+
+    SEX_CHOICES = (
+        (Male, "Male")
+        (Female, "Female")
+        (Other, "Other")
+    )
+    """
+    Default custom user model for dukka_assessment.
+    If adding fields that need to be filled at user signup,
+    check forms.SignupForm and forms.SocialSignupForms accordingly.
+    """
+
+    #: First and last name do not cover name patterns around the globe
+    name = CharField(_("Full name"), max_length=255)
+    first_name = None  # type: ignore
+    last_name = None  # type: ignore
+    username = None  # type: ignore
+
+    email = EmailField(_('email address'), unique=True)
+    sex = CharField(_("Gender"), max_length=1, choices=SEX_CHOICES)
+    country = CharField(_("Country"), max_length=255)
+    phone_number = PhoneNumberField(_("Phone number"), blank=True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+
+
+    def get_absolute_url(self):
+        """Get url for user's detail view.
+
+        Returns:
+            str: URL for user detail.
+
+        """
+        return reverse("users:detail", kwargs={"email": self.email})
